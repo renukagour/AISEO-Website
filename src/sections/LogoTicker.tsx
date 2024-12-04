b72b4b83a7b5f6f9bb0983504569eb626ffee6cc
Renuka Gour <2110041@ritindia.edu>
2024-12-04 14:12:43 +0530

    animation done

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+"use client"
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 export const LogoTicker = () => {
   return (
     <section className="py-20 md:py-24">
@@ -14,9 +16,23 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
+          <div className=" flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div 
+            initial={{translateX:"-50%"}}
+            animate={{translateX:"0"}}
+            transition={{
+              duration:30,
+              repeat:Infinity,
+              ease: "linear",
+            }}
+            className="flex flex-none gap-14 pr-10 -translate-x-1/2">
               {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestialLogo,
+                apexLogo,
+                quantumLogo,
                 acmeLogo,
                 pulseLogo,
                 echoLogo,
@@ -26,7 +42,7 @@ export const LogoTicker = () => {
               ].map((logo) => {
                 return <img src={logo.src} key={logo.src} className="h-6 w-auto " />;
               })}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
